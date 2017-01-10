57ca814846cd0d43469472d7c2441d39731b827a
dmariewll <dmariewll@gmail.com>
2017-01-10 13:27:11 -0500

    Ruby-data day 2

---
+++ part1.rb
@@ -0,0 +1,8 @@
+# PART 1: Words from strings
+# Create an array of the words in the sentence below
+# Find how many words have a length equal to 5
+
+text = "Tacos must not have lettuce Only meat and cheese and salsa"
+words = text.split
+
+puts words
